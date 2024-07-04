1467cc069c17d1977399cdcfd79415e4e6fc82ba
DevMaffi <devmaffi550@gmail.com>
2024-07-04 16:06:30 +0300

    Add utility types

---
+++ src/types/utils/DeepNonNullable.ts
@@ -0,0 +1,5 @@
+export type DeepNonNullable<T> = T extends object
+    ? {
+          [P in keyof T]-?: DeepNonNullable<T[P]>
+      }
+    : NonNullable<T>
---
+++ src/types/utils/index.ts
@@ -0,0 +1,7 @@
+export type { DeepNonNullable } from './DeepNonNullable'
+export type { DeepPartial } from './DeepPartial'
+export type { DeepReadonly } from './DeepReadonly'
+
+export type { Prettify } from './Prettify'
+
+export type { ValueOf } from './ValueOf'
