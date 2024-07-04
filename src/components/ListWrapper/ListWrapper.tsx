fd8fc3655b5517897eac2b380b236cf732c68b8d
DevMaffi <devmaffi550@gmail.com>
2024-07-04 16:12:36 +0300

    Add ListWrapper component

---
+++ src/components/ListWrapper/ListWrapper.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type ListWrapperProps<T> = Readonly<{
+    children: React.ReactNode
+    data: T[]
+}>
+
+function ListWrapper<T>(props: ListWrapperProps<T>) {
+    const { children, data } = props
+
+    const isEmpty = data.length === 0
+
+    if (isEmpty) {
+        return null
+    }
+
+    return children
+}
+
+export default ListWrapper
