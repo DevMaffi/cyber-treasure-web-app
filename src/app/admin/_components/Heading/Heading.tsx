8e38f6cc66f73cef6743888cffd38bdd44fbd691
DevMaffi <devmaffi550@gmail.com>
2024-07-09 12:54:19 +0300

    Add admin Heading cmp

---
+++ src/app/admin/_components/Heading/Heading.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { cn } from '@/lib/utils'
+import styles from './Heading.module.css'
+
+type HeadingProps = Readonly<React.ComponentProps<'h2'>>
+
+function Heading(props: HeadingProps) {
+    const { children, className, ...restProps } = props
+
+    return (
+        <h2 className={cn(styles.subtle, className)} {...restProps}>
+            {children}
+        </h2>
+    )
+}
+
+export default Heading
